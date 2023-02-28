adf01b3d0db405820db40df9391f740ac9ded4c7
APTIL9 <126561743+APTIL9@users.noreply.github.com>
2023-02-28 17:26:22 -0500

    Create utils.py

---
+++ utils.py
@@ -0,0 +1,57 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split, GridSearchCV
+from sklearn.svm import SVC
+from sklearn.ensemble import RandomForestClassifier, GradientBoostingClassifier
+from sklearn.metrics import confusion_matrix, classification_report, accuracy_score, roc_curve, auc
+
+def load_data(file_path):
+    data = pd.read_csv(file_path)
+    X = data.iloc[:, :-1].values
+    y = data.iloc[:, -1].values
+    return X, y
+
+  def preprocess_data(X, y):
+    scaler = StandardScaler()
+    X = scaler.fit_transform(X)
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
+    smote = SMOTE()
+    X_train, y_train = smote.fit_resample(X_train, y_train)
+    return X_train, X_test, y_train, y_test
+
+  def train_models(X_train, y_train):
+    models = {}
+    models['SVM'] = SVC(kernel='rbf', random_state=42)
+    models['Random Forest'] = RandomForestClassifier(random_state=42)
+    models['Gradient Boosting'] = GradientBoostingClassifier(random_state=42)
+    
+    for name, model in models.items():
+        model.fit(X_train, y_train)
+        
+    scores = {}
+    for name, model in models.items():
+        y_pred = model.predict(X_test)
+        score = accuracy_score(y_test, y_pred)
+        scores[name] = score
+        
+    return models, scores
+
+  def evaluate_models(models, X_test, y_test):
+    plt.figure(figsize=(8, 6))
+    for name, model in models.items():
+        y_pred_proba = model.predict_proba(X_test)[:, 1]
+        fpr, tpr, _ = roc_curve(y_test, y_pred_proba)
+        auc_score = auc(fpr, tpr)
+        plt.plot(fpr, tpr, label=f'{name} (AUC = {auc_score:.2f})')
+    
+    plt.plot([0, 1], [0, 1], 'k--')
+    plt.xlim([0.0, 1.0])
+    plt.ylim([0.0, 1.05])
+    plt.xlabel('False Positive Rate')
+    plt.ylabel('True Positive Rate')
+    plt.title('ROC Curve')
+    plt.legend(loc='lower right')
+    plt.show()
